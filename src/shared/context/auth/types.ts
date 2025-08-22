99e97459e687013e03bba9b452aaf23e138d04c5
Ovett Mora <ovettmora17@gmail.com>
2025-08-22 17:59:06 -0600

    Add AuthContext and useAuth hook for authentication management

---
+++ src/shared/context/auth/types.ts
@@ -0,0 +1,11 @@
+import type { User } from "@/modules/auth/types/user.types";
+
+export interface AuthContextType {
+  isAuthenticated: boolean;
+  isUserAdmin: () => boolean;
+  user: User | null;
+  loading: boolean;
+  login: (user: User) => void;
+  logout: () => Promise<void>;
+  updateUser: (userUpdated: User) => void;
+}
