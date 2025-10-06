bc660074fb15151ff08d1dd27fec534ce3b89f10
MarBHC <bustamantemaria078@gmail.com>
2025-10-05 21:38:47 -0600

    chore(main): setup React Query provider and default query options

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60, 
+    },
+  },
+});
 
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </StrictMode>
+);
