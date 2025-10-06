c9f28f4c9deba9d8e46555f8611cd0dcce5222d9
MarBHC <bustamantemaria078@gmail.com>
2025-10-05 21:10:59 -0600

    feat(hooks): add hook to fetch appointment by id

---
+++ src/modules/doctors/hooks/useAppointmentById.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAppointmentById } from "../api/doct.api"; 
+import type { AppointmentRes } from "@/shared/types/appointment.type";
+
+export const useAppointmentById = (id: number) => {
+  return useQuery<AppointmentRes, Error>({
+    queryKey: ["appointment", id],
+    queryFn: () => getAppointmentById(id),
+    enabled: Number.isFinite(id) && id > 0, 
+    staleTime: 1000 * 60, 
+  });
+};
