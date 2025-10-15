8b996bc4d55e73818432dec7a9ea7fbe24893982
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:25:21 -0600

    fix: update mutation error type from Error to ProcessedError in usePatientById hook

--- src/modules/doctors/hooks/usePatientById.ts
+++ src/modules/doctors/hooks/usePatientById.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPatientById } from "../api/doct.api";
-import type { Patient } from "@/shared/types/patient.types";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
+import type { Patient } from "@/modules/patient/types/patient.types";
 
 
 export const usePatientById = (id: string) => {
-  return useQuery<Patient, Error>({
+  return useQuery<Patient, ProcessedError>({
     queryKey: ["patient", id], 
     queryFn: () => getPatientById(id),
     enabled: !!id,            
