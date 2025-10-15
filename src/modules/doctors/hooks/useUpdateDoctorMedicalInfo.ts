ba52db1b6a6d754a7f41ddedc0a5cfe94e436259
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:26:24 -0600

    fix: update mutation error type from Error to ProcessedError in useUpdateDoctorMedicalInfo hook

--- src/modules/doctors/hooks/useUpdateDoctorMedicalInfo.ts
+++ src/modules/doctors/hooks/useUpdateDoctorMedicalInfo.ts
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateDoctorMedicalInfo } from "../api/doct.api";
 import type { DoctorMedicalInfo } from "../types/doctor.medicalinfo";
-import type { Doctor } from "@/shared/types/doctor.types";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
+import type { Doctor } from "../types/doctor.types";
 
 export const useUpdateDoctorMedicalInfo = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<Doctor, Error, { userId: string; request: DoctorMedicalInfo }>({
+  return useMutation<Doctor, ProcessedError, { userId: string; request: DoctorMedicalInfo }>({
     mutationFn: ({ userId, request }) => updateDoctorMedicalInfo(userId, request),
     onSuccess: (updatedDoctor) => {
 
