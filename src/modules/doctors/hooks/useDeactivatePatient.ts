20cab43dda4ecde4a673ae854c30b8a997af291e
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:24:20 -0600

    fix: update mutation error type from Error to ProcessedError in useDeactivatePatient hook

--- src/modules/doctors/hooks/useDeactivatePatient.ts
+++ src/modules/doctors/hooks/useDeactivatePatient.ts
@@ -1,10 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deactivatePatient } from "../api/doct.api";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useDeactivatePatient = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<string, Error, string>({
+  return useMutation<string, ProcessedError, string>({
     mutationFn: (id: string) => deactivatePatient(id),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["patients"] });
