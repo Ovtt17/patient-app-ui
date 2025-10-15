bec9a2df15cec81ebf2aa9aa5d98a07472f806ca
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:26:17 -0600

    fix: update mutation error type from Error to ProcessedError in useUpdateAppointmentStatus hook

--- src/modules/doctors/hooks/useUpdateAppointmentStatus.ts
+++ src/modules/doctors/hooks/useUpdateAppointmentStatus.ts
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateAppointmentStatus } from "../api/doct.api";
 import type { AppointmentStatus } from "@/shared/types/appointment.type";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useUpdateAppointmentStatus = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<void, Error, { appointmentId: number; status: AppointmentStatus }>({
+  return useMutation<void, ProcessedError, { appointmentId: number; status: AppointmentStatus }>({
     mutationFn: ({ appointmentId, status }) =>
       updateAppointmentStatus(appointmentId, status),
 
