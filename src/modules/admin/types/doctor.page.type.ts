38aa19c66f02eecf3439d4e66a730778f707055e
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 23:07:26 -0600

    refactor: remove unused DoctorPagedRes type definition

--- src/modules/admin/types/doctor.page.type.ts
+++
@@ -1,9 +0,0 @@
-import type { Doctor } from "@/modules/doctors/types/doctor.types";
-
-
-export interface DoctorPagedRes {
-  doctors: Doctor [];
-  page: number;
-  totalPages: number;
-  totalElements: number;
-}
