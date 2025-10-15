bc619ad82b4605bd3a33503a61dceb12aca753e4
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:25:48 -0600

    fix: update mutation error type from Error to ProcessedError in useSchedulesByDoctor hook

--- src/modules/doctors/hooks/useSchedulesByDoctor.ts
+++ src/modules/doctors/hooks/useSchedulesByDoctor.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getSchedulesByDoctor } from "../api/doct.api";
 import type { DayOfWeek,ScheduleRes } from "../types/schedule.types";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useSchedulesByDoctor = (doctorId: string, dayOfWeek?: DayOfWeek) => {
-  return useQuery<ScheduleRes[], Error>({
+  return useQuery<ScheduleRes[], ProcessedError>({
     queryKey: ["schedules", doctorId, dayOfWeek],
     queryFn: () => getSchedulesByDoctor(doctorId, dayOfWeek),
     enabled: !!doctorId, 
