efdcdc1dcd7c69dc7fee902251de2bc4b6edaade
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:24:55 -0600

    fix: update mutation error type from Error to ProcessedError in useDoctorAvailabilityByDoctorId hook

--- src/modules/doctors/hooks/useDoctorAvailabilityByDoctorId.ts
+++ src/modules/doctors/hooks/useDoctorAvailabilityByDoctorId.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getDoctorAvailabilityByDoctorId } from "../api/doct.api"; 
 import type { DoctorAvailabilityRes } from "../types/doctorAvailability.type";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useDoctorAvailabilityByDoctorId = (doctorId: string) => {
-  return useQuery<DoctorAvailabilityRes, Error>({
+  return useQuery<DoctorAvailabilityRes, ProcessedError>({
     queryKey: ["doctor-availability", doctorId],
     queryFn: () => getDoctorAvailabilityByDoctorId(doctorId),
     enabled: !!doctorId,       
