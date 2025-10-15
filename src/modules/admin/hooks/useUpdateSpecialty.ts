0072be99a8c1662701bec9d1a927d821568d5449
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:15:44 -0600

    fix: update mutation error type from Error to ProcessedError in useUpdateSpecialty hook

--- src/modules/admin/hooks/useUpdateSpecialty.ts
+++ src/modules/admin/hooks/useUpdateSpecialty.ts
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateSpecialty } from "../api/admin.api"; 
 import type { SpecialtyReq, SpecialtyRes } from "../../doctors/types/specialty.type";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useUpdateSpecialty = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<SpecialtyRes, Error, { id: number; request: SpecialtyReq }>({
+  return useMutation<SpecialtyRes, ProcessedError, { id: number; request: SpecialtyReq }>({
     mutationFn: ({ id, request }) => updateSpecialty(id, request),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["specialties"] });
