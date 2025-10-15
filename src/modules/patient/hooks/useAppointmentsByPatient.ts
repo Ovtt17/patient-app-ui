4670276e388cec96e81b79640c4c2fad8158df4e
MarBHC <bustamantemaria078@gmail.com>
2025-10-14 21:28:57 -0600

    fix: update mutation error type from Error to ProcessedError in useAppointmentsByPatient hook

--- src/modules/patient/hooks/useAppointmentsByPatient.ts
+++ src/modules/patient/hooks/useAppointmentsByPatient.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAppointmentsByPatient } from "../api/patient.api";
 import type { AppointmentRes } from "@/shared/types/appointment.type";
+import type { ProcessedError } from "@/modules/errors/types/exception-response.types";
 
 export const useAppointmentsByPatient = (patientId: string, fromDate?: string) => {
-  return useQuery<AppointmentRes[], Error>({
+  return useQuery<AppointmentRes[], ProcessedError>({
     queryKey: ["appointments", "patient", patientId, fromDate],
     queryFn: () => getAppointmentsByPatient(patientId, fromDate),
     enabled: Boolean(patientId),     
