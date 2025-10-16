a36bd61f360264633bebe6f0647807e0c97e8d13
Ovett Mora <ovettmora17@gmail.com>
2025-10-16 13:50:24 -0600

    feat: add doctor routes to AppRoutes with role-based access control

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -6,8 +6,9 @@ import ProtectedRoutes from "./ProtectedRoute";
 import authRoutes from "@/modules/auth/routes/auth.routes";
 import protectedRoutes from "./protected.routes";
 import publicRoutes from "@/router/public.routes.tsx";
-import {Role} from "@/modules/auth/types/role.types.ts";
+import { Role } from "@/modules/auth/types/role.types.ts";
 import adminRoutes from "@/modules/admin/routes/admin.routes.tsx";
+import doctorRoutes from "@/modules/doctors/routes/doctor.routes";
 
 const AppRoutes = () => {
   const { loading } = useAuth();
@@ -20,27 +21,36 @@ const AppRoutes = () => {
     <Routes>
       {/* Rutas públicas */}
       {publicRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
+        <Route key={route.path} path={route.path} element={route.element} />
       ))}
 
       {/* Rutas de autenticación (login, registro, etc.) */}
       {authRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
+        <Route key={route.path} path={route.path} element={route.element} />
       ))}
 
       {/* Rutas protegidas para cualquier usuario autenticado */}
-      <Route element={<ProtectedRoutes  />}>
+      <Route element={<ProtectedRoutes />}>
         {protectedRoutes.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
+          <Route key={route.path} path={route.path} element={route.element} />
         ))}
       </Route>
 
       {/* Rutas protegidas por rol: ADMIN */}
       <Route
-          element={ <ProtectedRoutes allowedRoles={[Role.ADMIN]} redirectPath={ROUTES.LOGIN} /> }
+        element={<ProtectedRoutes allowedRoles={[Role.ADMIN]} redirectPath={ROUTES.LOGIN} />}
       >
         {adminRoutes.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Route>
+
+      {/* Rutas protegidas por rol: DOCTOR */}
+      <Route
+        element={<ProtectedRoutes allowedRoles={[Role.DOCTOR]} redirectPath={ROUTES.LOGIN} />}
+      >
+        {doctorRoutes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
         ))}
       </Route>
 
