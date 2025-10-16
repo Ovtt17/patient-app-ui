f1747ffb63cee5fbcad19dcccb6568ab52544e39
Ovett Mora <ovettmora17@gmail.com>
2025-10-16 13:35:22 -0600

    feat: remove unused Home route from protected routes definition

--- src/router/protected.routes.tsx
+++ src/router/protected.routes.tsx
@@ -1,9 +1,6 @@
-import { Routes as ROUTES } from "@/shared/constants/routes";
-import Home from '@/modules/home/pages/Home';
 import type { RouteObject } from "react-router-dom";
 
 const protectedRoutes: RouteObject[] = [
-  { path: ROUTES.HOME, element: <Home /> },
 ];
 
 export default protectedRoutes;
