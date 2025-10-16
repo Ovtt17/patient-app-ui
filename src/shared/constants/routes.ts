8557b6499a77ea0494deded792ba10de92e7fbdf
Ovett Mora <ovettmora17@gmail.com>
2025-10-16 13:34:14 -0600

    feat: define public and admin routes; implement role-based access control

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -9,19 +9,6 @@ export const Routes = {
 
 } as const;
 
-export const RoutesAdmin = {
-  ADMIN_DOCTORS: "/admin/doctors",
-  ADMIN_DOCTORES_CREATE : "/admin/doctors/create",
-  ADMIN_DOCTOR_SCHEDULES : "/admin/doctors/schedules",
-  ADMIN_SPECIALTIES : "/admin/specialties",
-  ADMIN_PATIENTS : "/admin/patients",
-  ADMIN_SPECIALTIES_CREATE : "/admin/specialties/create",
-  ADMIN_SPECIALTIES_EDIT : "/admin/specialties/edit/:id",
-  ADMIN_DESACTIVATE_PATIENT : "/admin/patients/deactivate/:id",
-
-
-} as const;
-
 export const RoutesDoctor = {
   
 } as const;
