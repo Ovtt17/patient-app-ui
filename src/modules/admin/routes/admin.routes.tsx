7f1a209514edb23bf14f95cd3cb88adfe1c66493
Ovett Mora <ovettmora17@gmail.com>
2025-10-19 16:16:57 -0600

    fix: swap routes for doctor creation and listing to correct path assignments

--- src/modules/admin/routes/admin.routes.tsx
+++ src/modules/admin/routes/admin.routes.tsx
@@ -2,25 +2,25 @@ import type { RouteObject } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "@/shared/components/Loader/Loader";
 import { RoutesAdmin } from "@/modules/admin/routes/RoutesAdmin.ts";
-import Patient from "@/modules/patient/pages/Patient";
 
 const DoctorCreate = lazy(() => import("../pages/DoctorCreate"));
 const DoctorListPage = lazy(() => import("../pages/DoctorListPage"));
+const Patient = lazy(() => import("@/modules/patient/pages/Patient"));
 
 const adminRoutes: RouteObject[] = [
-  {
-    path: RoutesAdmin.ADMIN_DOCTORES_CREATE,
-    element: (
-      <Suspense fallback={<Loader />}>
-        <DoctorCreate />
-      </Suspense>
-    ),
-  },
   {
     path: RoutesAdmin.ADMIN_DOCTORS,
     element: (
       <Suspense fallback={<Loader />}>
         <DoctorListPage />
+      </Suspense>
+    ),
+  },
+  {
+    path: RoutesAdmin.ADMIN_DOCTORS_CREATE,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <DoctorCreate />
       </Suspense>
     ),
   },
