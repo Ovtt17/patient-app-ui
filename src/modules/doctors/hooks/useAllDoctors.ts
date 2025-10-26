b06f92421bbcd36c6236182eba79e73d0144e8e3
Ovett Mora <ovettmora17@gmail.com>
2025-10-25 23:33:24 -0600

    feat: add usePaginatedDoctors hook for paginated doctor retrieval

--- src/modules/doctors/hooks/useAllDoctors.ts
+++ src/modules/doctors/hooks/useAllDoctors.ts
@@ -1,70 +1,7 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-import { usePagination } from "@/shared/hooks/usePagination";
-import { initializePaginationParams } from "@/shared/utils/urlParamsUtils";
-import { usePaginatedEntities } from "@/shared/hooks/usePaginatedEntities";
-import type { DoctorResponse } from "@/modules/doctors/types/DoctorResponse";
-import type { SortOrder } from "@/shared/types/SortOrder";
+import { useAllEntities } from "@/shared/hooks/useAllEntities";
 import { getAllDoctors } from "../api/doctor.api";
-
+import type { DoctorResponse } from "../types/DoctorResponse";
 
 export const useAllDoctors = () => {
-  const prefix = "doctors";
-  const defaultSize = 20;
-
-  const { page, size, setPagination } = usePagination(prefix);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  // Inicializa los parámetros de paginación al cargar el componente
-  useEffect(() => {
-    const params = initializePaginationParams(
-      new URLSearchParams(searchParams),
-      prefix,
-      defaultSize
-    );
-    setSearchParams(params);
-  }, [searchParams, setSearchParams]);
-
-  const sortBy = searchParams.get("sortBy") || "createdDate";
-  const sortOrder: SortOrder = (searchParams.get('sortOrder') as SortOrder) || 'asc';
-
-
-  const {
-    entities: doctors,
-    totalPages,
-    totalElements,
-    loading,
-    error,
-    refetch,
-  } = usePaginatedEntities<DoctorResponse>({
-    key: prefix,
-    page,
-    size,
-    sortBy,
-    sortOrder,
-    fetcher: getAllDoctors
-  });
-
-  const handlePageChange = (page: number) => {
-    setPagination(page, size);
-  }
-
-  const handleSizeChange = (newSize: number) => {
-    const firstPage = 1;
-    setPagination(firstPage, newSize);
-  }
-
-  return {
-    doctors,
-    page,
-    size,
-    totalPages,
-    totalElements,
-    loading,
-    error,
-    refetch,
-    setPagination,
-    handlePageChange,
-    handleSizeChange
-  };
-};
+  return useAllEntities<DoctorResponse>('doctors', getAllDoctors);
+}
