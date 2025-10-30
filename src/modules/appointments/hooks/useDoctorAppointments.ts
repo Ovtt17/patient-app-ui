32f7a150b4606c14ee32d4a7250ed23a5504a3a3
Ovett Mora <ovettmora17@gmail.com>
2025-10-29 20:37:30 -0600

    feat: add appointment filtering functionality with doctor and patient selection

--- src/modules/appointments/hooks/useDoctorAppointments.ts
+++
@@ -1,9 +0,0 @@
-import { getAppointmentsByDoctor } from "../api/appointment.api";
-import { useAppointmentsBase } from "./useAppointmentsBase";
-
-export const useDoctorAppointments = (doctorId: string, fromDate?: Date) => {
-  return useAppointmentsBase({
-    queryKey: ['appointments', 'doctor', doctorId, fromDate ? fromDate.toISOString() : ''],
-    fetchFn: () => getAppointmentsByDoctor(doctorId, fromDate),
-  });
-};
--- src/modules/appointments/hooks/usePatientAppointments.ts
+++
@@ -1,9 +0,0 @@
-import { getAppointmentsByPatient } from '../api/appointment.api';
-import { useAppointmentsBase } from './useAppointmentsBase';
-
-export const usePatientAppointments = (patientId: string, fromDate?: Date) => {
-  return useAppointmentsBase({
-    queryKey: ['appointments', 'patient', patientId, fromDate ? fromDate.toISOString() : ''],
-    fetchFn: () => getAppointmentsByPatient(patientId, fromDate),
-  });
-};
